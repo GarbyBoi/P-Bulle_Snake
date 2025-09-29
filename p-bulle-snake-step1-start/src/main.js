bd728b7e0a05fd420aef490dd186a43343a4ffee
RaBaFID2 <119532130+GarbyBoi@users.noreply.github.com>
2025-09-29 09:19:26 +0200

    fix(main.js): fixed 2 mves in one interval problem
    implemented a direction queue to avoid 2 moves in one interval such as the snake being able to do U-turns on itself

--- p-bulle-snake-step1-start/src/main.js
+++ p-bulle-snake-step1-start/src/main.js
@@ -8,15 +8,22 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 const box = 20;
-const gameSpeed = 200;
+const gameSpeed = 170;
+const directionQueue = [];//gère 2 moves en même temps dans le même intervale
+
 let snake;
 let food;
 let direction = "RIGHT";
 let score = 0;
-let gameInterval; // Variable pour stocker l'identifiant de l'intervalle
+let gameInterval; // Variable pour stocker l'identifiant de l'intervalles
 
 document.addEventListener("keydown", (event) => {
-  direction = handleDirectionChange(event, direction);
+  const lastDirection = directionQueue.length > 0 ? directionQueue[directionQueue.length - 1] : direction;
+  const newDirection = handleDirectionChange(event, lastDirection);
+  //gère les double mouvements dans le même intervale
+  if (newDirection !== lastDirection) {
+    directionQueue.push(newDirection);
+  }
 });
 
 function startGame() {
@@ -28,6 +35,10 @@ function startGame() {
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  if (directionQueue.length > 0) {
+  direction = directionQueue.shift();//gère les mouvements dans l'ordre
+}
   const head = moveSnake(snake, direction, box);
 
   //si la tête et la nouritures partage les mêmes cordonnées, +1 de score et nouvelle pomme, pas de pop le snake grandi
